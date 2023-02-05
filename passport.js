d158c5aec87a72237aa939acdf5002f2ef948b79
sam19992 <sam.kumari12@gmail.com>
2023-02-05 21:29:16 +0530

    cloud url

--- passport.js
+++ passport.js
@@ -11,7 +11,8 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
 	clientID:"949673200369-d4euf4msko9rvole58d4buta0ecj77k6.apps.googleusercontent.com",
 	clientSecret:"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",
-	callbackURL:"http://localhost:8080/auth/callback",
+	// callbackURL:"http://localhost:8080/auth/callback",
+	callbackURL:"https://freecodeapp.onrender.com/auth/callback",
 	passReqToCallback:true
 },
 function(request, accessToken, refreshToken, profile, done) {
